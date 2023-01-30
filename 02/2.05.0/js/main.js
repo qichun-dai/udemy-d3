e111ee411c25d257c0ac03af5bb03ced60a70283
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2023-01-30 22:37:18 +0100

    test

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -3,3 +3,20 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+const svg= d3.select("#chart-area")
+.append("svg")
+
+svg.append("rect")
+.attr("x",40)
+.attr("y",0)
+.attr("width",200)
+.attr("height",100)
+.attr("fill","blue")
+
+
+svg.append("circle")
+.attr("cx",100)
+.attr("cy",100)
+.attr("r",20)
+.attr("fill","yellow")
