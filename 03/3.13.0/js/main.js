c6a57d3b627c11f757b1c61d455754abeab1fa09
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2023-02-07 21:26:26 +0100

    test

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -3,6 +3,9 @@
 *    Mastering Data Visualization with D3.js
 *    Project 1 - Star Break Coffee
 */
+const x = d3.scaleLinear()
+    .domain([0, 5000])    // INPUT!
+    .range([0, WIDTH]);  // OUTPUT!
 
 const MARGIN={LEFT: 40, RIGHT: 20, TOP: 20, BOTTOM: 30}
 const WIDTH= 600 - MARGIN.LEFT - MARGIN.RIGHT
@@ -15,8 +18,18 @@ const g = d3.select("#chart-area")
 .append("g")
 .attr("transform",`translate(${MARGIN.LEFT},${MARGIN.TOP})`)
 
-
+const topAxisCall = d3.axisTop(x)
+ 
+g.append("g")
+    .attr("class", "top axis")
+    .call(topAxisCall);
+    
 d3.csv("data/revenues.csv").then(data => {
+    data.forEach(d => {
+        d.revenue=Number(d.revenue);
+        d.profit=Number(d.profit);
+    });
+
     console.log(data)
 
 })
